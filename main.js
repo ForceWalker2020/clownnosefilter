c57892af54961de0573f0bf45d20a91978c0860d
ForceWalker2020 <84183620+ForceWalker2020@users.noreply.github.com>
2022-03-04 19:19:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 300, 300);
-    image(clown_nose, noseX, noseY, 50, 50);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot()
